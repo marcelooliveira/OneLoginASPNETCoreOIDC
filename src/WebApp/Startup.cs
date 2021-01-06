63c4d6cccc2f62a652e51aaffbd9473cbb133fa5
marcelo.r.oliveira <marcelo.r.oliveira@ebradi.com.br>
2021-01-06 12:43:08 -0300

    OpenIdConnect

--- src/WebApp/Startup.cs
+++ src/WebApp/Startup.cs
@@ -12,6 +12,9 @@ using WebApp.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using System.Security.Claims;
 
 namespace WebApp
 {
@@ -35,6 +38,36 @@ namespace WebApp
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+
+
+            // Allow sign in via an OpenId Connect provider like OneLogin
+            services.AddAuthentication(options => {
+                options.DefaultAuthenticateScheme = IdentityConstants.ApplicationScheme;
+                options.DefaultChallengeScheme = IdentityConstants.ApplicationScheme;
+                options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+            })
+            .AddCookie(options => {
+                options.LoginPath = "/Account/Login/";
+            })
+            .AddOpenIdConnect(options =>
+            {
+                options.ClientId = Configuration["oidc:onelogin-clientid"];
+                options.ClientSecret = Configuration["oidc:onelogin-clientsecret"];
+                options.Authority = "https://openid-connect.onelogin.com/oidc";
+                
+                options.ResponseType = "code";
+                options.GetClaimsFromUserInfoEndpoint = true;
+
+                options.SaveTokens = true;
+                options.Events = new OpenIdConnectEvents()
+                {
+                    OnTicketReceived = (context) =>
+                    {
+                        ClaimsIdentity identity = (ClaimsIdentity)context.Principal.Identity;
+                        return Task.CompletedTask;
+                    }
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
